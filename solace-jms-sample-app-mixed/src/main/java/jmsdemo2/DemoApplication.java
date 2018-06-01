e926b2a232d96a6bd3a093a9dce5b7411cf9a53e
Emman <emman.sun@cargosmart.com>
2018-06-01 17:49:59 +0800

    try properties

--- solace-jms-sample-app-mixed/src/main/java/jmsdemo2/DemoApplication.java
+++ solace-jms-sample-app-mixed/src/main/java/jmsdemo2/DemoApplication.java
@@ -16,115 +16,111 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessagePostProcessor;
 import org.springframework.jms.listener.SubscriptionNameProvider;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@SpringBootApplication
-public class DemoApplication {
+import javax.jms.JMSException;
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
+@SpringBootApplication public class DemoApplication {
 
-    @Service
-    static class MessageProducer implements CommandLineRunner {
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
 
-        private static final Logger logger = LoggerFactory.getLogger(MessageProducer.class);
+  @Service static class MessageProducer implements CommandLineRunner {
 
-        @Autowired()
-        @Qualifier("jndiJmsTemplate")
-        private JmsTemplate jmsTemplate;
+    private static final Logger logger = LoggerFactory.getLogger(MessageProducer.class);
 
-      @Autowired()
-      @Qualifier("jmsPublisher")
-      private JmsTemplate publisher;
+    @Autowired() @Qualifier("jndiJmsTemplate") private JmsTemplate jmsTemplate;
 
-      @Autowired()
-      @Qualifier("jndiJmsPublisher")
-      private JmsTemplate jndiPublisher;
+    @Autowired() @Qualifier("jmsPublisher") private JmsTemplate publisher;
 
-        /*
-        For backwards compatibility:
-        - As before, these exist only in the specific scenario where the app is deployed in Cloud Foundry.*/
-        @Autowired(required=false) private SolaceMessagingInfo solaceMessagingInfo;
+    @Autowired() @Qualifier("jndiJmsPublisher") private JmsTemplate jndiPublisher;
 
-        @Value("${solace.jms.demoConsumerQueueJndiName}")
-        private String queueName;
+    /*
+    For backwards compatibility:
+    - As before, these exist only in the specific scenario where the app is deployed in Cloud Foundry.*/
+    @Autowired(required = false) private SolaceMessagingInfo solaceMessagingInfo;
 
-      @Value("${solace.jms.demoTopicName}")
-      private String topicName;
+    @Value("${solace.jms.demoConsumerQueueJndiName}") private String queueName;
 
-      @Value("${solace.jms.demoTopicName2}")
-      private String topicName2;
+    @Value("${solace.jms.demoTopicName}") private String topicName;
 
-        @Override
-        public void run(String... strings) throws Exception {
-            String msg = "Hello World";
+    @Value("${solace.jms.demoTopicName2}") private String topicName2;
 
-          logger.info("============= Sending to topic " + topicName + ", msg=" + msg);
-          this.publisher.convertAndSend(topicName, msg);
-
-          logger.info("============= Sending to queue " + queueName  +", msg="+ msg);
-          try {
-            this.jmsTemplate.convertAndSend(queueName, msg);
-          } catch (Exception e) {
-            logger.error("failed to send msg to queue", e);
-          }
-
-          logger.info("============= Sending to topic " + topicName2 +", msg="+ msg);
-          try {
-            this.publisher.convertAndSend(topicName2, msg);
-          } catch (Exception e) {
-            logger.error("failed to send msg to topic endpoint", e);
-          }
+    @Override public void run(String... strings) throws Exception {
+      String msg = "Hello World";
+      MessagePostProcessor postProcessor = new MessagePostProcessor() {
+        @Override public javax.jms.Message postProcessMessage(javax.jms.Message message)
+          throws JMSException {
+          message.setBooleanProperty("JMS_Solace_isXML", false);
+          message.setIntProperty("Solace_JMS_CompressionLevel", 1);
+          return message;
         }
+      };
+      logger.info("============= Sending to topic " + topicName + ", msg=" + msg);
+      this.publisher.convertAndSend(topicName, msg, postProcessor);
+
+      logger.info("============= Sending to queue " + queueName + ", msg=" + msg);
+      try {
+        this.jmsTemplate.convertAndSend(queueName, msg, postProcessor);
+      } catch (Exception e) {
+        logger.error("failed to send msg to queue", e);
+      }
+
+      logger.info("============= Sending to topic " + topicName2 + ", msg=" + msg);
+      try {
+        this.publisher.convertAndSend(topicName2, msg, postProcessor);
+      } catch (Exception e) {
+        logger.error("failed to send msg to topic endpoint", e);
+      }
     }
+  }
 
 
-  @Component
-  static class MessageHandler {
+  @Component static class MessageHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(MessageHandler.class);
 
     // Retrieve the name of the queue from the application.properties file
-    @JmsListener(destination = "${solace.jms.demoConsumerQueueJndiName}",containerFactory="cFactory")
+    @JmsListener(destination = "${solace.jms.demoConsumerQueueJndiName}", containerFactory = "cFactory")
     public void processMsg(Message msg) {
       StringBuffer msgAsStr = new StringBuffer("============= Received \nHeaders:");
       MessageHeaders hdrs = msg.getHeaders();
-      msgAsStr.append("\nUUID: "+hdrs.getId());
-      msgAsStr.append("\nTimestamp: "+hdrs.getTimestamp());
+      msgAsStr.append("\nUUID: " + hdrs.getId());
+      msgAsStr.append("\nTimestamp: " + hdrs.getTimestamp());
       Iterator<String> keyIter = hdrs.keySet().iterator();
       while (keyIter.hasNext()) {
         String key = keyIter.next();
-        msgAsStr.append("\n"+key+": "+hdrs.get(key));
+        msgAsStr.append("\n" + key + ": " + hdrs.get(key));
       }
-      msgAsStr.append("\nPayload: "+msg.getPayload());
+      msgAsStr.append("\nPayload: " + msg.getPayload());
       logger.info(msgAsStr.toString());
     }
   }
 
 
-  @Component
-  static class TopicMessageHandler {
+  @Component static class TopicMessageHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(TopicMessageHandler.class);
 
     // Retrieve the name of the queue from the application.properties file
-    @JmsListener(destination = "${solace.jms.demoTopicName2}",containerFactory="tFactory", concurrency="1", subscription="${solace.jms.demoSubscriptionName}")
+    @JmsListener(destination = "${solace.jms.demoTopicName2}", containerFactory = "tFactory", concurrency = "1", subscription = "${solace.jms.demoSubscriptionName}")
     public void processMsg(Message msg) {
       StringBuffer msgAsStr = new StringBuffer("============= Received \nHeaders:");
       MessageHeaders hdrs = msg.getHeaders();
-      msgAsStr.append("\nUUID: "+hdrs.getId());
-      msgAsStr.append("\nTimestamp: "+hdrs.getTimestamp());
+      msgAsStr.append("\nUUID: " + hdrs.getId());
+      msgAsStr.append("\nTimestamp: " + hdrs.getTimestamp());
       Iterator<String> keyIter = hdrs.keySet().iterator();
       while (keyIter.hasNext()) {
         String key = keyIter.next();
-        msgAsStr.append("\n"+key+": "+hdrs.get(key));
+        msgAsStr.append("\n" + key + ": " + hdrs.get(key));
       }
-      msgAsStr.append("\nPayload: "+msg.getPayload());
+      msgAsStr.append("\nPayload: " + msg.getPayload());
       logger.info(msgAsStr.toString());
     }
   }
--- solace-jms-sample-app-mixed/src/main/java/jmsdemo2/ProducerConfiguration.java
+++ solace-jms-sample-app-mixed/src/main/java/jmsdemo2/ProducerConfiguration.java
@@ -13,61 +13,53 @@ import org.springframework.jms.support.destination.JndiDestinationResolver;
 import org.springframework.jndi.JndiObjectFactoryBean;
 import org.springframework.jndi.JndiTemplate;
 
-@Configuration
-public class ProducerConfiguration {
-	@Value("${solace.jms.demoConnectionFactoryJndiName}")
-	private String connectionFactoryJndiName;
-	@Autowired private JndiTemplate jndiTemplate;
+@Configuration public class ProducerConfiguration {
+  @Value("${solace.jms.demoConnectionFactoryJndiName}") private String connectionFactoryJndiName;
+  @Autowired private JndiTemplate jndiTemplate;
 
-	@Bean
-	public JndiObjectFactoryBean connectionFactory() {
-		JndiObjectFactoryBean factoryBean = new JndiObjectFactoryBean();
-		factoryBean.setJndiTemplate(jndiTemplate);
-		factoryBean.setJndiName(connectionFactoryJndiName);
-		return factoryBean;
-	}
+  @Bean public JndiObjectFactoryBean connectionFactory() {
+    JndiObjectFactoryBean factoryBean = new JndiObjectFactoryBean();
+    factoryBean.setJndiTemplate(jndiTemplate);
+    factoryBean.setJndiName(connectionFactoryJndiName);
+    return factoryBean;
+  }
 
-	@Bean
-	public CachingConnectionFactory cachingConnectionFactory() {
-		CachingConnectionFactory ccf = new CachingConnectionFactory((ConnectionFactory) connectionFactory().getObject());
-		ccf.setSessionCacheSize(10);
-		return ccf;
-	}
+  @Bean public CachingConnectionFactory cachingConnectionFactory() {
+    CachingConnectionFactory ccf =
+      new CachingConnectionFactory((ConnectionFactory) connectionFactory().getObject());
+    ccf.setSessionCacheSize(10);
+    return ccf;
+  }
 
-	// Example use of CachingConnectionFactory for the producer
-	@Bean
-	public JmsTemplate jmsTemplate() {
-		return new JmsTemplate(cachingConnectionFactory());
-	}
+  // Example use of CachingConnectionFactory for the producer
+  @Bean public JmsTemplate jmsTemplate() {
+    return new JmsTemplate(cachingConnectionFactory());
+  }
 
-	@Bean
-	public JmsTemplate jmsPublisher() {
-		JmsTemplate jmsTemplate =  new JmsTemplate(cachingConnectionFactory());
-		jmsTemplate.setPubSubDomain(true);
-		return jmsTemplate;
-	}
+  @Bean public JmsTemplate jmsPublisher() {
+    JmsTemplate jmsTemplate = new JmsTemplate(cachingConnectionFactory());
+    jmsTemplate.setPubSubDomain(true);
+    return jmsTemplate;
+  }
 
-	@Bean
-	public JndiDestinationResolver jndiDestinationResolver() {
-		JndiDestinationResolver jdr = new JndiDestinationResolver();
-		jdr.setCache(true);
-		jdr.setJndiTemplate(jndiTemplate);
-		return jdr;
-	}
+  @Bean public JndiDestinationResolver jndiDestinationResolver() {
+    JndiDestinationResolver jdr = new JndiDestinationResolver();
+    jdr.setCache(true);
+    jdr.setJndiTemplate(jndiTemplate);
+    return jdr;
+  }
 
-	@Bean
-	public JmsTemplate jndiJmsTemplate() {
-		JmsTemplate jmsTemplate =  new JmsTemplate(cachingConnectionFactory());
-		jmsTemplate.setDestinationResolver(jndiDestinationResolver());
-		return jmsTemplate;
-	}
+  @Bean public JmsTemplate jndiJmsTemplate() {
+    JmsTemplate jmsTemplate = new JmsTemplate(cachingConnectionFactory());
+    jmsTemplate.setDestinationResolver(jndiDestinationResolver());
+    return jmsTemplate;
+  }
 
-	@Bean
-	public JmsTemplate jndiJmsPublisher() {
-		JmsTemplate jmsTemplate =  new JmsTemplate(cachingConnectionFactory());
-		jmsTemplate.setDestinationResolver(jndiDestinationResolver());
-		jmsTemplate.setPubSubDomain(true);
-		return jmsTemplate;
-	}
+  @Bean public JmsTemplate jndiJmsPublisher() {
+    JmsTemplate jmsTemplate = new JmsTemplate(cachingConnectionFactory());
+    jmsTemplate.setDestinationResolver(jndiDestinationResolver());
+    jmsTemplate.setPubSubDomain(true);
+    return jmsTemplate;
+  }
 
 }
